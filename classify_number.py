6ab8ed61ff8881b1f8cc58f323269ce74feafa0d
gonzaloferreyra2007 <gferreyra3@mail.austral.edu.ar>
2026-04-06 18:06:38 -0300

    Add files via upload

--- classify_number.py
+++ classify_number.py
@@ -4,9 +4,13 @@ def is_even(n):
     """Dado un número entero n, retorna True si es par, False si es impar."""
     return n % 2 == 0
 
+
 def is_positive(n):
     """Dado un número entero n, retorna True si es mayor a 0, False en caso contrario."""
-    return n > 0
+    if n > 0:
+        return True
+    else:
+        return False
 
 # ---- Función a implementar ----
 
@@ -22,4 +26,14 @@ def classify_number(n):
       - "negative odd"    (negativo e impar)
       - "zero"            (el número es 0)
     """
-    return "ANSWER HERE"  # Remove this line and implement
+
+    if n == 0:
+        return "zero"
+    if is_positive(n) and is_even(n):
+        return "positive even"
+    elif is_positive(n) and not is_even(n):
+        return "positive odd"
+    elif not is_positive(n) and is_even(n):
+        return "negative even"
+    else:
+        return "negative odd"
